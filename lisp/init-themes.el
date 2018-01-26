ed4992978050395a33872b0f1efb6c76d72cdfa4
Andrea <cazzola.andrea@gmail.com>
2018-01-26 14:14:36 +0000

    Added functions to switch between light/dark themes

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,8 +1,30 @@
-(use-package leuven-theme
-  :ensure t)
+(package-install 'zenburn-theme)
+(package-install 'leuven-theme)
 
-(use-package zenburn-theme
-  :ensure t)
+;; load themes without enabling them and without asking for confirmation
+(load-theme 'leuven t t)
+(load-theme 'zenburn t t)
 
+(defvar current-dark-theme 'zenburn)
+(defvar current-light-theme 'leuven)
+
+(defun dark-theme ()
+  "Enable dark theme of choice."
+  (interactive)
+  (message "Going dark")
+  (disable-theme current-light-theme)
+  (enable-theme current-dark-theme)
+  )
+
+(defun light-theme ()
+  "Enable light-theme of choice."
+  (interactive)
+  (message "There be light")
+  (disable-theme current-dark-theme)
+  (enable-theme current-light-theme)
+  )
+
+(dark-theme)
 
 (provide 'init-themes)
+;;; init-themes ends here
