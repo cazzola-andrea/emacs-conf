7d0429a13ce6b00c82efb48ee6f73d6279792a70
Andrea <acazzola@lastline.com>
2019-07-10 14:03:00 +0100

    Added swith to send mail to local server. Changed mail sorting.

--- lisp/init-mail.el
+++ lisp/init-mail.el
@@ -9,6 +9,9 @@
   :config
   (setq gnus-parameters'((".*" (display . all))))
   (setq gnus-thread-indent-level 1)
+  (setq gnus-thread-sort-functions
+        '((not gnus-thread-sort-by-number)
+          (not gnus-thread-sort-by-date)))
   (define-key gnus-group-mode-map "q" 'bury-buffer)
   )
 
@@ -21,9 +24,25 @@
 	       (nnimap-server-port "imaps")
 	       (nnimap-stream ssl)))
 
-(setq smtpmail-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-service 587
-      gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
+(setq message-send-mail-function 'smtpmail-send-it)
 
+(defun ac/set-smtp-gmail ()
+  "Set up mail client to send mail w Gmail."
+  (interactive)
+  (setq smtpmail-smtp-server "smtp.gmail.com"
+        smtpmail-smtp-service 587
+        gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]"
+        )
+)
+
+(defun ac/set-smtp-sensor ()
+  "Set up smtp client to send to LL sensor."
+  (interactive)
+  (setq smtpmail-smtp-server "sensor-cloud"
+        smtpmail-smtp-service 25)
+  )
+
+(ac/set-smtp-gmail)
+  
 (provide 'init-mail)
 ;;; init-mail.el ends here
